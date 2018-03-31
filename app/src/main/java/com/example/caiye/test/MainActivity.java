13e2ea503d6a3ad6edba6300b9c5900546fe7189
Caiyeee <caiye29@mail2.sysu.edu.cn>
2018-04-01 02:53:24 +0800

    完成主页面和好友列表页面

--- app/src/main/java/com/example/caiye/test/MainActivity.java
+++ app/src/main/java/com/example/caiye/test/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.caiye.test;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -28,6 +33,7 @@ import com.iflytek.cloud.ui.RecognizerDialogListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +42,16 @@ public class MainActivity extends AppCompatActivity {
     private Button btnRecorder;
     private Button btnRecorderEnd;
     private String fullText = " ";
+    private TextView name;
+    private TextView tags;
+    private FloatingActionButton jumpToFriends;
+    private ImageView youandme;
+    private Intent intentToShow;
+    private Person person;
+    private List<Map<String,String>> recommend;
+    private SimpleAdapter simpleAdapter;
+    private String getName;
+    private String getTag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,11 +59,52 @@ public class MainActivity extends AppCompatActivity {
         SpeechUtility.createUtility(this, SpeechConstant.APPID + "=5abf120b");
         setContentView(R.layout.activity_main);
 
-
-
+        recommend = new ArrayList<Map<String, String>>();
+        name = (TextView) findViewById(R.id.name_now);
+        tags = (TextView)findViewById(R.id.tags);
+        youandme = (ImageView) findViewById(R.id.youandme);
         btnRecorder = (Button) findViewById(R.id.btn_record);
         btnRecorderEnd = (Button) findViewById(R.id.btn_end);
         listview = (ListView) findViewById(R.id.listview);
+        jumpToFriends = (FloatingActionButton) findViewById(R.id.jumpToFriends);
+
+
+        intentToShow = getIntent();
+        if(intentToShow!=null){
+            getName = intentToShow.getStringExtra("name");
+            getTag = intentToShow.getStringExtra("tags_init");
+        }
+
+        if(getName==null){
+            name.setText("None");
+            youandme.setAlpha(255);
+        } else {
+            name.setText(getName);
+            youandme.setAlpha(100);
+            //获取推荐，放到recommend中
+            /////////////////////////////////////////////////////////////////////
+        }
+
+        if(getTag==null)
+            tags.setText("您沒有對這位朋友設置標籤喔！");
+        else
+            tags.setText(getTag);
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,recommend,R.layout.friend_item,
+                new String[]{"title",""},new int[]{R.id.firstLetter,R.id.name});
+        simpleAdapter.notifyDataSetChanged();
+        listview.setAdapter(simpleAdapter);
+
+        //点击悬浮按钮跳转到好友列表页面
+        jumpToFriends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,FriendsList.class);
+                startActivity(intent);
+            }
+        });
+
+        //开始录音
         btnRecorder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

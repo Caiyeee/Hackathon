4490388d75d29978f30571a80575bb4269302fb1
zsy <djytyang@gmail.com>
2018-04-01 01:31:50 +0800

    using ArrayAdapt to getData

--- app/src/main/java/com/example/caiye/test/MainActivity.java
+++ app/src/main/java/com/example/caiye/test/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.caiye.test;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -19,23 +19,27 @@ import com.iflytek.cloud.SpeechUtility;
 import com.iflytek.cloud.ui.RecognizerDialog;
 import com.iflytek.cloud.ui.RecognizerDialogListener;
 
-import com.iflytek.cloud.RecognizerResult;
-import com.iflytek.cloud.SpeechConstant;
-import com.iflytek.cloud.SpeechError;
-import com.iflytek.cloud.SpeechUtility;
-import com.iflytek.cloud.ui.RecognizerDialog;
-import com.iflytek.cloud.ui.RecognizerDialogListener;
-
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private ListView listview;
+    private ListView listView;
     private Button btnRecorder;
     private Button btnRecorderEnd;
     private String fullText = " ";
+
+    private List<String> getData() {
+
+        List<String> data = new ArrayList<String>();
+        data.add("测试数据1");
+        data.add("测试数据2");
+        data.add("测试数据3");
+        data.add("测试数据4");
+
+        return data;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +48,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnRecorder = (Button) findViewById(R.id.btn_record);
         btnRecorderEnd = (Button) findViewById(R.id.btn_end);
-        listview = (ListView) findViewById(R.id.listview);
+        listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,
+                getData()));
         btnRecorder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
